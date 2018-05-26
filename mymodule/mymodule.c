11fc9e8dd1aee6747db85dceda00f9b3f1e08c3c
fhanisch <felix.hanisch@live.de>
2018-05-27 00:19:11 +0200

    array und arraybuffer

--- mymodule/mymodule.c
+++ mymodule/mymodule.c
@@ -44,7 +44,8 @@ napi_value timesTwo(napi_env env, napi_callback_info info) {
 }
 
 napi_value myObject(napi_env env, napi_callback_info info) {
-
+	double *x;
+	napi_value buf;
 	napi_status status = napi_generic_failure;
 
 	// const obj = {}
@@ -54,7 +55,6 @@ napi_value myObject(napi_env env, napi_callback_info info) {
 
 	// Create a napi_value for 123
 	status = napi_create_int32(env, 123, &value);
-	
 
 	// obj.myProp = 123
 	status = napi_set_named_property(env, obj, "myProp", value);
@@ -62,6 +62,10 @@ napi_value myObject(napi_env env, napi_callback_info info) {
 	status = napi_create_string_utf8(env, "Horst", strlen("Horst"), &value);
 	status = napi_set_named_property(env, obj, "Vorname", value);
 
+	// Create ArrayBuffer
+	napi_create_arraybuffer(env, 40, (void**)&x, &buf);
+	x[0] = 1.2345; x[1] = 344.009293; x[2] = -2332.99283; x[3] = -0.239488; x[4] = 521409348.3299;
+	status = napi_set_named_property(env, obj, "doublearr", buf);
 	return obj;
 }
 
@@ -71,8 +75,10 @@ napi_value readObject(napi_env env, napi_callback_info info) {
 	size_t argc = 1;
 	napi_value argv[1];
 	napi_value result;
+	napi_value element;
 	char buf[128];
 	size_t numbytes;
+	uint32_t len;
 
 	status = napi_get_cb_info(env, info, &argc, argv, NULL, NULL);
 	if (status != napi_ok) {
@@ -81,8 +87,21 @@ napi_value readObject(napi_env env, napi_callback_info info) {
 
 	napi_get_named_property(env, argv[0], "name", &result);
 	napi_get_value_string_utf8(env, result, buf, 128, &numbytes);
-
 	printf("%s\n",buf);
+
+	napi_get_named_property(env, argv[0], "strarr", &result);
+	napi_get_array_length(env, result, &len);
+	printf("length = %u\n", len);
+
+	napi_get_element(env, result, 0, &element);
+	napi_get_value_string_utf8(env, element, buf, 128, &numbytes);
+	printf("%s\n", buf);
+	napi_get_element(env, result, 1, &element);
+	napi_get_value_string_utf8(env, element, buf, 128, &numbytes);
+	printf("%s\n", buf);
+	napi_get_element(env, result, 2, &element);
+	napi_get_value_string_utf8(env, element, buf, 128, &numbytes);
+	printf("%s\n", buf);
 	return NULL;
 }
 
--- mymodule/mymodule.js
+++ mymodule/mymodule.js
@@ -11,8 +11,13 @@ console.log(addon.timesTwo(12.5));
 var obj = addon.myObject();
 console.log(obj.myProp);
 console.log(obj.Vorname);
+console.log(obj.doublearr);
 console.log(addon);
+var x = new Float64Array(obj.doublearr, 0, 5);
+console.log(x);
 
+Person.strarr = ["juhu", "jippi", "guten tag"];
+console.log(Person.strarr);
 addon.readObject(Person);
 
 console.log(test.timestwo(7));
