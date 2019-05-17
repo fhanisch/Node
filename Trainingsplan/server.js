a309cd7bd88a657caa91d9d6430cfbd7ff8509a8
fhanisch <felix.hanisch@live.de>
2019-05-17 22:55:54 +0200

    data files

--- Trainingsplan/server.js
+++ Trainingsplan/server.js
@@ -2,6 +2,7 @@ const http = require('http');
 const url = require('url');
 var fs = require('fs');
 
+//const hostname = "127.0.0.1";
 const hostname = "192.168.1.2";
 const port = 3000;
 
@@ -12,29 +13,8 @@ function send(res, buf, contentType)
     res.end();
 }
 
-const server = http.createServer(function(req, res){
-	var q = url.parse(req.url, true);
-	console.log(req.method);
-	console.log(req.url);
-	//console.log(q);
-	var filename;
-	var contentType;
-	if (req.url=="/") 
-	{
-		filename = "index.html";
-		contentType = "text/html";
-	}
-	else if (req.url=="/style.css")
-	{
-		filename = "style.css";
-		contentType = "text/css";
-	}
-	else 
-	{
-		res.statusCode = 404;
-		res.end();
-		return;
-	}
+function sendFile(res, filename, contentType)
+{
 	fs.readFile(filename, function(err, fdata) {
 		if (err) {
 			res.statusCode = 404;
@@ -43,6 +23,33 @@ const server = http.createServer(function(req, res){
 		}
 		send(res, fdata, contentType);
 	});
+}
+
+const server = http.createServer(function(req, res){
+	var q = url.parse(req.url, true);
+	console.log(req.method);
+	console.log(req.url);
+	console.log(q.pathname);
+	var filename;
+	var contentType;
+	if (req.url=="/") 
+	{
+		sendFile(res, "index.html", "text/html");
+	}
+	else if (req.url=="/style.css")
+	{
+		sendFile(res, "style.css", "text/css");
+	}
+	else if (q.pathname == "/getTable")
+	{
+		console.log("getTable");
+		sendFile(res, "plan2.json", "application/json")
+	}
+	else 
+	{
+		res.statusCode = 404;
+		res.end();
+	}
 });
 
 server.listen(port, hostname, function(){
